d19512b8b67690f4147150bff4a873affc792596
xameeramir <zameeramir@gmail.com>
2023-04-15 20:39:34 +0530

    Explore page updated

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -9,10 +9,15 @@ function About() {
             <h1>About us</h1>
             <p>Allow us to tell you a story...</p>
 
-            <AboutCard title="Chapter 1" desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"/>
-            <AboutCard title="Chapter 2" desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"/>
-            <AboutCard title="Chapter 3" desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"/>
-            <AboutCard title="Chapter 4" desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"/>
+            <div className='about-cards'>
+                <AboutCard title="Chapter 1" desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book" />
+                <AboutCard title="Chapter 2" desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book" />
+            </div>
+
+            <div className='about-cards'>
+                <AboutCard title="Chapter 3" desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book" />
+                <AboutCard title="Chapter 4" desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book" />
+            </div>
         </div>
     );
 }
--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -4,9 +4,18 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <div className="footer">
-      Footer
-    </div>
+    <div className='footer'>
+      <div className='nav-items'>
+        <div className='logo'>Your Logo</div>
+      </div>
+      <div className='nav-items'>
+        <div className='nav-item'>Explore</div>
+        <div className='nav-item'>About Us</div>
+        <div className='nav-item'>Cities <span className='cities-dropdown' /> </div>
+        <div className='nav-item nav-item-outlined'>Call</div>
+      </div>
+    </ div>
+
   );
 }
 
